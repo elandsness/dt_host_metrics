cee3216002b2c31096f46f7c467e9f64493cf377
elandsness <elandsness@gmail.com>
2019-06-26 17:12:39 -0700

    Added config to handle metric definitions

--- bin.js
+++ bin.js
@@ -1,9 +1,17 @@
 const fetchdata = require('.')
 
+const METRIC_OPTIONS = require('./metrics.js');
+
+let epilog = [];
+for (var x in METRIC_OPTIONS){
+    epilog.push(`${x}: ${METRIC_OPTIONS[x].metric} = ${METRIC_OPTIONS[x].desc}`);
+}
+
 // Handle command line args
 var argv = require('yargs')
-    .usage('Usage: $0 -k [api key] -t [tenant url] -f [output file]')
+    .usage('Usage: $0 -k [api key] -t [tenant url] -f [output file] -m [select metrics 0,1,2,3,4,5,6,etc]')
     .demandOption(['k','t'])
+    .epilog(epilog.join("\n"))
     .argv;
 
 // Store api key, tenant url and file path in constants for later use
