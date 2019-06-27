494b600df8c4ed5dfdd788ea1e5c222e59c5ada0
elandsness <elandsness@gmail.com>
2019-06-26 19:15:40 -0700

    Standard host metrics working

--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@ const fetchdata = require('.')
 
 const METRIC_OPTIONS = require('./metrics.js');
 
-let epilog = [];
+let epilog = ['Available Metrics:'];
 for (var x in METRIC_OPTIONS){
     epilog.push(`${x}: ${METRIC_OPTIONS[x].metric} = ${METRIC_OPTIONS[x].desc}`);
 }
@@ -10,7 +10,7 @@ for (var x in METRIC_OPTIONS){
 // Handle command line args
 var argv = require('yargs')
     .usage('Usage: $0 -k [api key] -t [tenant url] -f [output file] -m [select metrics 0,1,2,3,4,5,6,etc]')
-    .demandOption(['k','t'])
+    .demandOption(['k','t','m'])
     .epilog(epilog.join("\n"))
     .argv;
 
@@ -18,5 +18,17 @@ var argv = require('yargs')
 const KEY = argv.k;
 const TENANT = argv.t[argv.t.length -1] == '/' ? argv.t.slice(0, -1) : argv.t; // strip trailing slash if there is one
 const FILE = argv.f ? argv.f : 'dt_data.csv';
+const METRICS = argv.m.split(',');
 
-fetchdata(KEY, TENANT, FILE);
+let fail = false;
+METRICS.forEach(element => {
+    if(parseInt(element) < 1 || parseInt(element) > Object.keys(METRIC_OPTIONS).length){
+        fail = true;
+    }
+  });
+if (fail){
+    console.log('Invalid metric selection! Please enter numbers between 1 and ' + Object.keys(METRIC_OPTIONS).length);
+    console.log('To see a list of available metrics, run this script again with the --help option added.');
+} else {
+    fetchdata(KEY, TENANT, FILE, METRICS);
+}
--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-module.exports = function(KEY, TENANT, FILE){
+module.exports = function(KEY, TENANT, FILE, METRICS){
     // HTTP client for making API calls
     const axios = require('axios');
 
+    // Metric definitions
+    const METRIC_OPTIONS = require('./metrics.js');
+
     // For outputting data to file
-    var fs = require('file-system');
+    const process_csv = require('./process_csv.js');
 
     // Object to store objects to build lines of csv
     let data = {};
 
-    // Query timeseries API for memory used
-    let endpoint = `${TENANT}/api/v1/timeseries/com.dynatrace.builtin:host.mem.used?includeData=true&relativeTime=day&aggregationType=avg&`;
+    // Arrays to split out the host vs timeseries vs custom metrics
+    let host_m = [], time_m = [], func_m = [];
+
+    // Split the desired metrics into host and time, as they're handled differently
+    METRICS.forEach(element => {
+        if (METRIC_OPTIONS[element].hasOwnProperty('function')) {
+            func_m.push(element);
+        } else if (METRIC_OPTIONS[element].type == 'host'){
+            host_m.push(element);
+        } else {
+            time_m.push(element);
+        }
+    });
+
+    // Query the host API to get the hostname and all the host metrics
+    let endpoint = `${TENANT}/api/v1/entity/infrastructure/hosts?relativeTime=day`;
+    axios.get(endpoint, {
+        headers: {
+            'Authorization': `Api-Token ${KEY}`,
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        // handle success
+        for (var x in response.data){
+            data[response.data[x].entityId] = {};
+            data[response.data[x].entityId].hostname = response.data[x].displayName
+            host_m.forEach(element => {
+                data[response.data[x].entityId][METRIC_OPTIONS[element].metric] = response.data[x][METRIC_OPTIONS[element].metric];
+            })
+        }
+    }).catch(function (error) {
+        // handle error
+        console.log(error.message);
+    }).finally(function () {
+        process_csv(data, METRICS, FILE);
+    });
+
+
+
+    /*// Query timeseries API for memory used
+    let endpoint = `${TENANT}/api/v1/timeseries/com.dynatrace.builtin:host.mem.used?includeData=true&relativeTime=day&aggregationType=avg`;
     axios.get(endpoint, {
         headers: {
             'Authorization': `Api-Token ${KEY}`,
@@ -30,10 +72,10 @@ module.exports = function(KEY, TENANT, FILE){
         }
     }).catch(function (error) {
         // handle error
-        console.log(error);
+        console.log(error.message);
     }).finally(function () {
         // Query timeseries API for % memory used
-        endpoint = `${TENANT}/api/v1/timeseries/com.dynatrace.builtin:host.mem.availablepercentage?includeData=true&relativeTime=day&aggregationType=avg&`;
+        endpoint = `${TENANT}/api/v1/timeseries/com.dynatrace.builtin:host.mem.availablepercentage?includeData=true&relativeTime=day&aggregationType=avg`;
         axios.get(endpoint, {
             headers: {
                 'Authorization': `Api-Token ${KEY}`,
@@ -51,7 +93,7 @@ module.exports = function(KEY, TENANT, FILE){
             }
         }).catch(function (error) {
             // handle error
-            console.log(error);
+            console.log(error.message);
         }).finally(function () {
             // Query the hosts API to get the consumed host units
             endpoint = `${TENANT}/api/v1/entity/infrastructure/hosts?relativeTime=day`;
@@ -69,7 +111,7 @@ module.exports = function(KEY, TENANT, FILE){
                 }
             }).catch(function (error) {
                 // handle error
-                console.log(error);
+                console.log(error.message);
             }).finally(function () {
                 // create the csv
                 let contents = ['Dynatrace_ID,Hostname,Memory_Used_GB,Memory_Used_%,Host_Units'];
@@ -77,14 +119,18 @@ module.exports = function(KEY, TENANT, FILE){
                     contents.push(`${x},${data[x].hostname},${data[x].used_gb},${data[x].used_percent},${data[x].host_units}`);
                 }
                 // write out the file
-                fs.writeFile(FILE, contents.join("\n"), function(err) {
-                    if (err){
-                        console.log(err)
-                    } else {
-                        console.log(`All Set! Open "${FILE}" to view results.`);
-                    }
-                });
+                if (contents.length < 2){
+                    console.log('Your query returned no data! Nothing to export.');
+                } else {
+                    fs.writeFile(FILE, contents.join("\n"), function(err) {
+                        if (err){
+                            console.log(err)
+                        } else {
+                            console.log(`All Set! Open "${FILE}" to view results.`);
+                        }
+                    });
+                }
             });
         });
-    });
+    });*/
 }
